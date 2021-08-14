9c37249d4edb6e36624ba03d75d8cb1b338081b8
ishworpanta10 <ishworpanta10@gmail.com>
2021-08-14 20:50:13 +0545

    project initialized

---
+++ android/app/src/main/kotlin/com/ishworpanta/flutter_notes_sqflite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ishworpanta.flutter_notes_sqflite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Notes App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
